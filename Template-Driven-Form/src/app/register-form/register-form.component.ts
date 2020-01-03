de66e15b9b804df9c521c9193f2e09cae7555975
DenislavVelichkov <denislav.velichkov@gmail.com>
2020-01-03 20:26:30 +0200

    Template Driven Form exercis.

---
+++ Template-Driven-Form/src/app/register-form/register-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.css']
+})
+export class RegisterFormComponent implements OnInit {
+
+  private areaCodes: Array<string> = ["+359", "+721", "+123"];
+  @ViewChild('registerForm', { static: false })
+  registerForm: NgForm;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  register(formData: FormData) {
+    if (this.registerForm.valid) {
+      console.log(formData);
+      this.registerForm.reset();
+    }
+
+  }
+
+}
